de587eff7265c23ff6599b4837a6a39681b72762
erc9510 <43975489+erc9510@users.noreply.github.com>
2019-02-08 18:20:39 -0500

    Add files via upload

--- tester.py
+++ tester.py
@@ -1,3 +1,14 @@
 import requests
-r =  requests.get("http://127.0.0.1")
-assert "Hello World" in r.text
+
+
+def test_page():
+	r =  requests.get("http://127.0.0.1")
+	assert "Hello World" in r.text
+	
+
+def main():
+	test_page()
+
+
+if __name__ == "__main__":
+	main()
